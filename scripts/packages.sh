df51111dc42d29998cbc83fe5d1a36c8ccfa711a
DipherL <88871987+DipherL@users.noreply.github.com>
2025-07-08 23:59:30 -0500

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S fastfetch nvim matugen-bin waypaper thorium-browser-bin flatpak zip unzip xarchiver nautilus nwg-dock-hyprland btop alacritty feh rofi-wayland swww npm swaync
+yay -S fastfetch nvim matugen-bin waypaper thorium-browser-bin flatpak zip unzip xarchiver nautilus nwg-dock-hyprland btop alacritty feh rofi-wayland swww npm swaync waybar 
