6942ae8ef5397ab00b5ea267822d8ddb70e32d28
DipherL <88871987+DipherL@users.noreply.github.com>
2025-07-09 00:24:39 -0500

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S fastfetch nvim matugen-bin waypaper ttf-meslo-nerd fzf wl-clipboard thorium-browser-bin flatpak zip unzip xarchiver nautilus nwg-dock-hyprland btop alacritty feh rofi-wayland swww npm swaync waybar starship
+yay -S fastfetch nvim matugen-bin waypaper ttf-meslo-nerd fzf nwg-look wl-clipboard thorium-browser-bin flatpak zip unzip xarchiver nautilus nwg-dock-hyprland btop alacritty feh rofi-wayland swww npm swaync waybar starship
